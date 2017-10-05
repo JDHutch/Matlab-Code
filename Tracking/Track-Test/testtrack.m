c79536f132acaedd21f743745af9a1ba735266af
Joe <31283352+JDHutch@users.noreply.github.com>
2017-10-05 16:46:48 +0100

    Particle finding seems to be working.

--- Tracking/Track-Test/testtrack.m
+++ Tracking/Track-Test/testtrack.m
@@ -1,9 +1,21 @@
 a = uint8(imread('c:\users\rdgroup\desktop\testtrack.tif'));
 %a = 255-a;
+a  = rgb2gray(a);
 
 colormap('gray'), imagesc(a);
 whos a
+hold on
 
+b = bpass(a,1,5);
+%colormap('gray'), image(b);
 
-b = bpass(a,1,10);
-colormap('gray'), image(b);
+pk = pkfnd(b, 60, 5);
+
+max(max(b))
+
+cnt = cntrd(b, pk, 7);
+whos cnt
+
+%colormap('jet')
+scatter(cnt(:,1), cnt(:,2), 8, 'filled')
+hold off

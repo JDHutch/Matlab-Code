c4b90b04c05b96b91c78c24dfc03137ed075454d
Joe <31283352+JDHutch@users.noreply.github.com>
2017-10-05 16:57:25 +0100

    Added comments to p-finding.

--- Tracking/Track-Test/testtrack.m
+++ Tracking/Track-Test/testtrack.m
@@ -1,21 +1,24 @@
-a = uint8(imread('c:\users\rdgroup\desktop\testtrack.tif'));
-%a = 255-a;
-a  = rgb2gray(a);
+a = uint8(imread('c:\users\rdgroup\desktop\testtrack.tif')); % Read in image as unsigned 8 bit int
 
-colormap('gray'), imagesc(a);
-whos a
-hold on
+a  = rgb2gray(a); % Set image to greyscale
 
-b = bpass(a,1,5);
+colormap('gray'), imagesc(a); % Get image up in pop-up
+%whos a % Information about variable a
+
+hold on % Any image/plot stuff after this point is an overlay of the initial image
+
+b = bpass(a,1,5); % Band pass -- input: image, ?, diameter of particle --
+whos b
 %colormap('gray'), image(b);
 
-pk = pkfnd(b, 60, 5);
+pk = pkfnd(b, 60, 5); % Particle finding -- input: processed image, brightness threshold, diameter --
+whos pk
 
-max(max(b))
+%max(max(b)) % Finds maximum brightness
 
-cnt = cntrd(b, pk, 7);
+cnt = cntrd(b, pk, 7); % Finds centre of gaussian to more precise value -- input: image, found particles, diameter --
 whos cnt
 
 %colormap('jet')
-scatter(cnt(:,1), cnt(:,2), 8, 'filled')
-hold off
+scatter(cnt(:,1), cnt(:,2), 8, 'filled') % Plots scatter of points onto original imagesc
+hold off % Stop overaly

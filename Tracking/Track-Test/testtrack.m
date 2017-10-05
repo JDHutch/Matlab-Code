94935d3ffa8f9e18c1c30c1378d3fb8079b2ca26
Joe <31283352+JDHutch@users.noreply.github.com>
2017-10-05 17:34:45 +0100

    Loop files
    Loop over image files for particle finding.

--- Tracking/Track-Test/testtrack.m
+++ Tracking/Track-Test/testtrack.m
@@ -1,24 +1,29 @@
-a = uint8(imread('c:\users\rdgroup\desktop\testtrack.tif')); % Read in image as unsigned 8 bit int
+i = 0;
+fmax = 1;
+for i=0:fmax
+  file =  strcat('c:\users\rdgroup\desktop\testtrack', num2str(i), '.tif');
+  a = uint8(imread(file)); % Read in image as unsigned 8 bit int
 
-a  = rgb2gray(a); % Set image to greyscale
+  a  = rgb2gray(a); % Set image to greyscale
 
-colormap('gray'), imagesc(a); % Get image up in pop-up
-%whos a % Information about variable a
+  colormap('gray'), imagesc(a); % Get image up in pop-up
+  %whos a % Information about variable a
 
-hold on % Any image/plot stuff after this point is an overlay of the initial image
+  hold on % Any image/plot stuff after this point is an overlay of the initial image
 
-b = bpass(a,1,5); % Band pass -- input: image, ?, diameter of particle --
-whos b
-%colormap('gray'), image(b);
+  b = bpass(a,1,5); % Band pass -- input: image, ?, diameter of particle --
+  whos b
+  %colormap('gray'), image(b);
 
-pk = pkfnd(b, 60, 5); % Particle finding -- input: processed image, brightness threshold, diameter --
-whos pk
+  pk = pkfnd(b, 60, 5); % Particle finding -- input: processed image, brightness threshold, diameter --
+  whos pk
 
-%max(max(b)) % Finds maximum brightness
+  %max(max(b)) % Finds maximum brightness
 
-cnt = cntrd(b, pk, 7); % Finds centre of gaussian to more precise value -- input: image, found particles, diameter --
-whos cnt
+  cnt = cntrd(b, pk, 7); % Finds centre of gaussian to more precise value -- input: image, found particles, diameter --
+  whos cnt
 
-%colormap('jet')
-scatter(cnt(:,1), cnt(:,2), 8, 'filled') % Plots scatter of points onto original imagesc
-hold off % Stop overaly
+  %colormap('jet')
+  scatter(cnt(:,1), cnt(:,2), 8, 'filled') % Plots scatter of points onto original images
+  hold off % Stop overaly
+end

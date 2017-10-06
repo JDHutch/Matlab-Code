259ae14bc5ac53627c5c0dcb1084b6ea327c343a
Joe <31283352+JDHutch@users.noreply.github.com>
2017-10-06 15:33:41 +0100

    Renamed file and started to get output.

--- Tracking/Track-Test/Particle-Finding.m
+++ Tracking/Track-Test/Particle-Finding.m
@@ -1,7 +1,20 @@
+% Purpose: Read in image files, find particles in image (needs to be tuned) and write out coordiante data over time
+
+imloc = 'c:\users\rdgroup\desktop\testtrack'; % Image file location
+imext = '.tif'; % Image file extension
+ofileloc = 'c:\users\rdgroup\desktop\'; % Output file location
+ofilename = 'outfile'; % Output file name
+
+t = 0; % Time beginning
+dt = 1; % Time between each frame
+
+
+cdata = zeros(0,5); % Array holds x, y, brightness, RoG^2, and time
 i = 0;
-fmax = 1;
+fmax = 1; % Number of images to go through
+
 for i=0:fmax
-  file =  strcat('c:\users\rdgroup\desktop\testtrack', num2str(i), '.tif');
+  file =  strcat(imloc, num2str(i), imext);
   a = uint8(imread(file)); % Read in image as unsigned 8 bit int
 
   a  = rgb2gray(a); % Set image to greyscale
@@ -25,5 +38,10 @@ for i=0:fmax
 
   %colormap('jet')
   scatter(cnt(:,1), cnt(:,2), 8, 'filled') % Plots scatter of points onto original images
+  % x,y,brightness, Square RoG
   hold off % Stop overaly
+
+  pnum = size(cnt)[1]; % Find how many rows there are
+  cnt = cat(2, cnt, (ones(1,pnum)*t)); % Concatenate vector with times to last column of cnt
+  cdata = cat(1, cdata, cnt);
 end
